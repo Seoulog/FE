008425fcd85d9591a6e86f2f67d5f43e884fa7d2
Minggurazi <jhsong1450@naver.com>
2024-01-12 02:04:19 +0900

    feat: user slice에 token 추가

--- src/lib/redux/slices/userSlice.ts
+++ src/lib/redux/slices/userSlice.ts
@@ -3,11 +3,15 @@ import { createSlice, type PayloadAction } from '@reduxjs/toolkit';
 interface IUserState {
   nickname: string;
   email: string;
+  isLogin: boolean;
+  accessToken: string;
 }
 
 const initialState: IUserState = {
   nickname: '',
-  email: ''
+  email: '',
+  isLogin: false,
+  accessToken: ''
 };
 
 export const userSlice = createSlice({
@@ -15,14 +19,37 @@ export const userSlice = createSlice({
   initialState,
   reducers: {
     SET_USER: (state, action: PayloadAction<IUserState>) => {
-      state = action.payload;
+      state.nickname = action.payload.nickname;
+      state.email = action.payload.email;
+      state.isLogin = action.payload.isLogin;
     },
     DELETE_USER: (state) => {
-      state = { nickname: '', email: '' };
+      state.email = '';
+      state.nickname = '';
+      state.isLogin = false;
+    },
+    SET_ISLOGIN: (state) => {
+      state.isLogin = true;
+    },
+    DELETE_ISLOGIN: (state) => {
+      state.isLogin = false;
+    },
+    SET_TOKEN: (state, action: PayloadAction<string>) => {
+      state.accessToken = action.payload;
+    },
+    DELETE_TOKEN: (state) => {
+      state.accessToken = '';
     }
   }
 });
 
-export const { SET_USER, DELETE_USER } = userSlice.actions;
+export const {
+  SET_USER,
+  DELETE_USER,
+  SET_ISLOGIN,
+  DELETE_ISLOGIN,
+  SET_TOKEN,
+  DELETE_TOKEN
+} = userSlice.actions;
 
 export default userSlice;
